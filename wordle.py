e396af4ddc0ec3574b4d78cd4e71cb9eeec52bf7
Jack Nelson <jackstn@icloud.com>
2024-08-22 17:52:36 +0000

    cleaned up codespace

--- wordle.py
+++ wordle.py
@@ -1,17 +1,12 @@
+#I tried to get it to randomly choose a word but wonderwords wont work D:<
 from wonderwords import RandomWord
-# TASK A: Define a variable 'word' that holds the correct word for the wordle game
-word = r.word(word_min_length=5, word_max_length=5)
-t = 1
+theWord = r.word(word_min_length=5, word_max_length=5)
+word = "beear"
 
-# TASK B: Define a function 'makeAGuess()' that passes in a users guess as a parameter
 def makeAGuess(userGuess):
   global word
 
-
-  # TASK C:Define a variable 'hint' that holds an empty string
   hint = ""
-
-  # TASK D: Build a loop that loops from 0 to the length of word
   for i in range(len(word)):
     if userGuess[i] == word[i]:
       hint += ("G")
@@ -20,23 +15,11 @@ def makeAGuess(userGuess):
     else:
       hint += ("-")
   return hint
-
-
-    # TASK E: Check if the current letter of guess matches the current letter of word. If so add the letter "G" to the hint
-    
-    
-    # TASK F: If the previous condition is fales, check if the current letter of guess is in word at all. If so add the letter "Y" to the hint
-    
-
-    # TASK G: If the previous two conditions are false, add the symbol "-" to the hint
-    
-  # TASK H: Return hint
   
 
 
 print("Let's play wordle! /n Guess the Wordle in 6 tries. Each guess must be a valid 5-letter word. For each guess, a hint will tell you how many letters you've guessed correctly. A G represents a letter in the word and in the correct spot.. A Y represents a letter in the word but in the wrong spot. A - represents a letter not in the word in any spot. \n Guess below! \n")
 
-# TASK I: Build a loop that loops 6 times (representing the number of guesses a user has)
 for i in range(6):
   guess = input("Put your guess here: ").lower()
 
@@ -48,16 +31,3 @@ for i in range(6):
     break
 if hint != "GGGGG":
   print("You Lost")
-
-
-
-  # TASK J: Define a variable 'guess'. prompt the user for their 5-letter guess and store it in the variable
-
-  # TASK K: Define a variable 'hint' and set the return of makeAGuess(guess) to that variable
-
-  # TASK L: Print hint
-
-  # TASK M: Check if hint = "GGGGG". If so the user has won. Print a win message and break the loop
-  
-
-# TASK N: After the loop has finished, meaning the user has run out of guesses, check if hint != "GGGGG". If so, the user has lost. Print a lose message. 
