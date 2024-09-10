08edc9953a1fc950a03d848907a60d110c006474
Jack Nelson <jackstn@icloud.com>
2024-09-10 15:43:25 +0000

    thing broke again

--- runner.py
+++ runner.py
@@ -1,11 +1,33 @@
 import sys
 import readchar
+import time
+import random
 from character import Character
 from item import Item
-from wordle import playWordle
 
+cash = 2000
 menuPlace = 1
+selectingShop = True
+playLoop = False
 
+events = {
+    "Guy with glasses": 1,
+    "Lady with hat": 1,
+    "Hungry businessman": 2,
+    "College student": 1,
+    "Food critic": 3,
+    "Tourist family": 2,
+    "Picky eater": 2,
+    "Vegetarian customer": 2,
+    "Food blogger": 3,
+    "Late-night partier": 1,
+    "Health inspector": 4,
+    "Celebrity in disguise": 3,
+    "Competitive eater": 2,
+    "Elderly couple": 1,
+    "Group of teenagers": 2
+}
+#got help from cody to generate the events
 def deleteLines(num_lines=1):
     for _ in range(num_lines):
         sys.stdout.write('\033[F')  # Move cursor up one line
@@ -27,39 +49,80 @@ def handle_key_press():
             menuPlace -= 1
         else:
             menuPlace = 4
-        deleteLines(5)
+        deleteLines(6)
     elif key == readchar.key.DOWN:
         print("Down key clicked")
         if menuPlace < 4:
             menuPlace += 1
         else:
             menuPlace = 1
-        deleteLines(5)
+        deleteLines(6)
     elif key == readchar.key.ESC:
         exit()
     elif key == readchar.key.ENTER:
         print("Enter key clicked")
+        selectingShop = True
         handle_menu_selection()
-    
     storeMenu()
 
 def handle_menu_selection():
-    if menuPlace == 1:
+    global selectingShop
+    global cash
+    if menuPlace == 1 and cash >= 10:
+        cash -= 10
+        selectingShop = False
+        deleteLines(6)
         print("You picked the Hot Dog Cart!")
-    elif menuPlace == 2:
+    elif menuPlace == 1 and cash < 10:
+        selectingShop = True
+        deleteLines(6)
+        print("You don't have enough money!")
+        time.sleep(1)
+        deleteLines(1)
+    elif menuPlace == 2 and cash >= 250:
+        cash -= 250
+        selectingShop = False
+        deleteLines(6)
         print("You picked the Hot Dog Stand!")
-    elif menuPlace == 3:
+    elif menuPlace == 2 and cash < 250:
+        selectingShop = True
+        deleteLines(6)
+        print("You don't have enough money!")
+        time.sleep(1)
+        deleteLines(1)
+    elif menuPlace == 3 and cash >= 650:
+        cash -= 650
+        selectingShop = False
+        deleteLines(6)
         print("You picked the Hot Dog Truck!")
-    elif menuPlace == 4:
+    elif menuPlace == 3 and cash < 650:
+        selectingShop = True
+        deleteLines(6)
+        print("You don't have enough money!")
+        time.sleep(1)
+        deleteLines(1)
+    elif menuPlace == 4 and cash >= 1500:
+        cash -= 1500
+        selectingShop = False
+        deleteLines(6)
         print("You picked the Hot Dog Store Front!")
+    elif menuPlace == 4 and cash < 1500:
+        selectingShop = True
+        deleteLines(6)
+        print("You don't have enough money!")
+        time.sleep(1)
+        deleteLines(1)
 
 def storeMenu():
-    menu_options = [
-        "[10]Hot Dog Cart",
-        "[250]Hot Dog Stand",
-        "[650]Hot Dog Truck",
-        "[1500]Hot Dog Store Front"
-    ]
+    global menu_options
+    if selectingShop == True:
+        print("Cash: " + str(cash))
+        menu_options = [
+            "[10]Hot Dog Cart",
+            "[250]Hot Dog Stand",
+            "[650]Hot Dog Truck",
+            "[1500]Hot Dog Store Front"
+        ]
     
     for i, option in enumerate(menu_options, 1):
         if i == menuPlace:
@@ -72,14 +135,13 @@ Pick your Hot Dog Selling Location""")
 
 storeMenu()
 
-while True:
+while selectingShop == True:
     handle_key_press()
 
-# The rest of your code remains the same
-d1 = input("You come up to a field and see a star! do you want to pick it up? (y/n)")
-if d1 == "y":
-    star = Item("Star", "A Shiny Star", 10)
-    print("You picked up the star! now you see a dude. want to talk to him? (y/n)")
-    d1a = input()
-    if d1a == "y":
-        character = Character("Dude", 10, [], {"Hello": "Hi! I'm a dude"})
+
+
+def eventHandler(event):
+    global playLoop
+    while playLoop == True:
+        event = random.choice(list(events.keys()))
+        eventHandler(event)
